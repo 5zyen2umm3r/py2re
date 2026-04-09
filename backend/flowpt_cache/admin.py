9d406bc98119cd0c60b5e77758876a04ed6ab314
am <summer0x414d@gmail.com>
2026-04-09 23:26:01 +0900

    adminの修正

--- backend/flowpt_cache/admin.py
+++ backend/flowpt_cache/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from .models import CachedEntity, EntityHistory, EntityDiff, Snapshot
+from .models import CachedEntity, EntityHistory, EntityDiff, Snapshot, CachedProject
 
 @admin.register(CachedEntity)
 class CachedEntityAdmin(admin.ModelAdmin):
     list_display = ["id", "entity_type", "flowpt_id"]
     list_filter = ["entity_type", "project"]
 @admin.register(EntityDiff)
-class CachedEntityAdmin(admin.ModelAdmin):
+class EntityDiffAdmin(admin.ModelAdmin):
     list_display = ["id", "entity_type", "flowpt_id", "patch"]
 
 # Register your models here.
 admin.site.register(EntityHistory)
 admin.site.register(Snapshot)
 
+admin.site.register(CachedProject)
